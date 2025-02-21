8e2c8a1b7887bea2bc61b3fe767d37c4e2b45899
Sean Berndlmaier <138079299+seanb1526@users.noreply.github.com>
2025-02-21 13:45:20 -0500

    Project Initialized and Firebase app initialized and connected.
    Beginning front end for authorization.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} /> 
       </Routes>
     </Router>
   );
